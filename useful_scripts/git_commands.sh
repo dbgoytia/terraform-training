903653e50a0a6008ce3e72e29c692c6db1e73056
Diego <diego.canizales1995@gmail.com>
2020-11-03 20:28:32 -0600

    Fixed: gitignore file for lab10.

--- useful_scripts/git_commands.sh
+++ useful_scripts/git_commands.sh
@@ -1,3 +1,10 @@
 # Fix a messed up branch
 git filter-branch --index-filter 'git rm -r --cached --ignore-unmatch <file/dir>' HEAD
 
+# Clone a branch from the main branch:
+git checkout -b dev main
+
+
+# Delete a branch
+git branch -d dev
+
